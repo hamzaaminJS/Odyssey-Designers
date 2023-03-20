1f605e784ffce0d2029826428bad96d2f04a494e
Hamza Amin <hamzawork.cs@gmail.com>
2023-03-20 20:15:31 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import './styles/main.scss'
 import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Switch, Route, Link } from 'react-router-dom';
 import Main from './pages/Main'
 import LandingPage from './pages/LandingPage';
 import EBook from './pages/EBook';
+import Another from './pages/Another';
 
 function App() {
   
-  const toggleDisplay = () => {
-    const popup = document.querySelector('.popup-container')
-      popup.style.display = 'flex'
-      setTimeout(() => {
-        popup.style.opacity = '1'
-      }, 1);
-}
+//   const toggleDisplay = () => {
+//     const popup = document.querySelector('.popup-container')
+//       popup.style.display = 'flex'
+//       setTimeout(() => {
+//         popup.style.opacity = '1'
+//       }, 1);
+// }
 
-const timeOutPopup = () => {
-    return setTimeout(() => {
-        toggleDisplay()
-    }, 4000);
-}
+// const timeOutPopup = () => {
+//     return setTimeout(() => {
+//         toggleDisplay()
+//     }, 4000);
+// }
 
-useEffect(() => {
-    timeOutPopup()
-}, [])
+// useEffect(() => {
+//     timeOutPopup()
+// }, [])
   return (
     <Router>
       <Routes>
+        <Route path="/another" element={<Another/>}/>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/e-book" element={<EBook/>}/>
       </Routes>
